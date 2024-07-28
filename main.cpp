4230c2a21606026623cf75c654f306cff064a686
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-07-28 14:32:15 +0300

    first working window

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <print>
+#include <iostream>
 
 using std::println;
 
@@ -18,7 +19,8 @@ auto main() -> int {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  GLFWwindow* main_window = glfwCreateWindow(480, 270, "First created GLFW window", NULL, NULL);
+  GLFWwindow* main_window =
+      glfwCreateWindow(960, 540, "OpenGL study", NULL, NULL);
   if (!main_window) {
     println(stderr, "GLFW windod creation failed");
     glfwTerminate();
@@ -27,4 +29,28 @@ auto main() -> int {
 
   int buffer_width, buffer_height;
   glfwGetFramebufferSize(main_window, &buffer_width, &buffer_height);
+
+  glfwMakeContextCurrent(main_window);
+
+  glewExperimental = GL_TRUE;
+
+  const GLenum err = glewInit();
+  if (err != GLEW_OK) {
+    println(stderr, "GLEW initialisation failed");
+    std::cerr << glewGetErrorString(err) << '\n';
+    glfwDestroyWindow(main_window);
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glViewport(0, 0, buffer_width, buffer_height);
+
+  while(!glfwWindowShouldClose(main_window)) {
+    glfwPollEvents();
+    glClearColor(1.0f, 0.0f, 1.0f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(main_window);
+  }
+
+  return EXIT_SUCCESS;
 }
